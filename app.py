2d93bae52ff3eb959d903fc75bda40f97cfdc0e4
Jose Santisteban <josesantisteban062@gmail.com>
2024-09-27 13:45:02 -0500

    cors update

--- app.py
+++ app.py
@@ -11,9 +11,26 @@ import torch.nn.functional as F
 from torch_geometric.loader import DataLoader
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Inicializar la aplicación FastAPI
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Credenciales y datos de conexión
 user = 'azure_admin'
